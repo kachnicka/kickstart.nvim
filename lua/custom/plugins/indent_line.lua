4254f4c67ed7b8ac3c17307122af953fa71693a4
kachnicka <m_kacerik@yahoo.com>
2024-05-18 00:37:50 +0200

    split plugin config

---
+++ lua/custom/plugins/indent_line.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    tag = 'v3.5.4',
+    main = 'ibl',
+    opts = {
+      indent = {
+        char = '▏',
+        -- char = '┊',
+      },
+    },
+  },
+}
