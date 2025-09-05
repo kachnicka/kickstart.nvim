f233e5cc37c4f4f2fac7a16e61a02a899a73e286
kachnicka <kachnicka.dev@gmail.com>
2025-09-05 13:09:45 +0200

    oil.nvim keybinds;

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -7,7 +7,14 @@ return {
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   config = function()
-    require('oil').setup()
+    require('oil').setup {
+      keymaps = {
+        ['<Esc>'] = { 'actions.close', mode = 'n' },
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    }
     vim.keymap.set('n', '-', '<cmd>Oil --float<CR>', { desc = 'Open parent dir' })
   end,
 }
